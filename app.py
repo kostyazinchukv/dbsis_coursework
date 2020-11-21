60ad68c094ef0175514d127de5281b3f075b4e7d
VladyslavHerasymenko18 <8889344@ukr.net>
2020-11-21 22:00:02 +0200

    Payment

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 
 app.config.from_object(Config)
-app.secret_key = 'Heil_Adolf_Hitler'
+# app.secret_key = 'Heil_Adolf_Hitler'
 
 
 @app.before_request
@@ -53,6 +53,96 @@ def send_polis():
 
    return render_template('payment.html', price=price1, area=area1)
 
+@app.route('/payment')
+def payment():
+
+   end_price = session['real_price']
+
+   text = f"""
+   
+      Dear Customer,
+
+      Please, make a payment of {end_price} uah to our requisites:
+      Card number: 1488 1488 1488 1488. Holder: Cum Dick Dickovich 
+
+      With kind regards,
+      CumDickCompany   
+   
+   """
+
+   send_email(session['email_user'],text)
+
+   return render_template('payment_health.html', price=session['real_price'])
+
+@app.route('/send_polis_health')
+def send_polis_health():
+
+   send_email(session['email_user'],text = session['contract_text'])
+   #session.pop('email_user',None)
+   #session.pop('contract_text', None)
+   price1 = session['real_price']
+   #session.pop('real_price',None)
+
+   return render_template('payment_health.html', price=price1)
+
+@app.route('/asswecan/<price>', methods=['GET', 'POST'])
+def form_health(price):
+   if g.user_id == None:
+      print('Спершу увійдіть') #TODO Message Flashing
+      return redirect(url_for('login'))
+   if request.method == 'GET':
+      return render_template('insurance_health_form.html', price=price)
+   else:
+      connection = psycopg2.connect(
+         app.config['SQLALCHEMY_DATABASE_URI']
+         )
+      connection.autocommit = True
+
+      session['price'] = request.form['tariff']
+
+      session['email_user'] = request.form['email']
+
+
+
+      if session['price'] == 'beginner':
+         real_price = 34
+      elif session['price'] == 'plusplus':
+         real_price = 45
+      elif session['price'] == 'pro':
+         real_price = 60
+      else:
+         raise Exception
+
+      session['real_price'] = real_price
+
+      second_name = request.form['second_name']
+      first_name = request.form['first_name']
+      third_name_kekw = request.form['third_name_kekw']
+
+      city = request.form['city']
+      town = request.form['town']
+      street = request.form['street']
+      session['birthday'] = request.form['birthday']
+
+
+      session['contract_text'] = f"""
+      Dear {second_name} {first_name} {third_name_kekw},
+
+      This e-mail is generated automatically and is sent to inform you about the terms of 
+      your {session['price']}-level insurance agreement. Those are the following:
+      The insurance covers health and life of our customer {second_name} {first_name} {third_name_kekw} born on {session['birthday']}, 
+      who lives in {city},{town},{street};
+      Insurance`s price will be {real_price} uah a day;
+
+      With kind regards,
+      CumDickCompany
+      """
+      create_contract(g.user_id, 'Life '+price, str(real_price), '2022-10-10', connection) # TODO Пофіксити на нормальну дату
+      connection.close()
+
+      return render_template('payment_health.html', price=real_price)
+
+
 @app.route('/new_contract/<price>', methods=['GET', 'POST'])
 def new_contract(price):
    if g.user_id == None:
@@ -81,6 +171,8 @@ def new_contract(price):
       else:
          raise Exception
 
+      session['real_price'] = real_price
+
       second_name = request.form['second_name']
       first_name = request.form['first_name']
       third_name_kekw = request.form['third_name_kekw']
@@ -101,7 +193,7 @@ def new_contract(price):
       With kind regards,
       CumDickCompany
       """
-      create_contract(g.user_id, price, real_price, '2022-10-10', connection) # TODO Пофіксити на нормальну дату
+      create_contract(g.user_id, 'Household '+price, str(real_price), '2022-10-10', connection) # TODO Пофіксити на нормальну дату
       connection.close()
 
       return render_template('payment.html', price=session['price'], area=session['area'])
@@ -155,9 +247,7 @@ def send_email(email, text = 'Tut bude infa pro polis (sorry, ne vstyg zapility)
    return render_template('mainpage.html')
 
 
-@app.route('/asswecan/<price>')
-def form_health(price):
-    return render_template('insurance_health_form.html', price=price)
+
 
 
 
